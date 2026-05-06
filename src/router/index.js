127c269c1a5df736ba93935180c06bfad9d5683c
KIM-HAK <hittheroad13299gmail.com>
2026-05-06 15:00:20 +0700

    dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
 import Homeview from '@/views/profile/Homeview.vue'
+import StaffDashboardView from '@/views/staff/StaffDashboardView.vue'
+import DashboardLayout from '@/views/staff/StaffDashboardView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+
     {
       path: '/',
       name: 'homeview',
       component: Homeview,
-    }
+    },
+
+    {
+      path: '/staff',
+      component: StaffdashboardView,
+      children: [
+        {
+          path: '/staffdashboardView',
+          name: 'staffDashboard',
+          component: StaffDashboardView,
+        },
+
+      ]
+    },
   ],
 })
 
