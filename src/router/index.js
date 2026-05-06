02ae657e1d2172405a65251c383da276dbeaa009
KIM-HAK <hittheroad13299gmail.com>
2026-05-06 20:59:44 +0700

    basemodal

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import Homeview from '@/views/profile/Homeview.vue'
+import StaffDashboardView from '@/views/staff/StaffDashboardView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -9,7 +10,13 @@ const router = createRouter({
       path: '/',
       name: 'homeview',
       component: Homeview,
+    },
+    {
+      path: '/staffdashboardview',
+      name: 'staffdashboardview',
+      component: StaffDashboardView,
     }
+
   ],
 })
 
